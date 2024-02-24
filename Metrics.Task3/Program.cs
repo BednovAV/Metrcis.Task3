558d0ea71c82bbfa9a3f89dc1f6ea14d94b48a25
Andrei Bednov <bednov_av@mail.ru>
2024-02-24 18:13:36 +0400

    completed task

---
+++ Metrics.Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+char[] divs = { '\n' };
+var codeLines = File.ReadAllText("code.txt").Split(divs, StringSplitOptions.RemoveEmptyEntries);
+var totalLines = codeLines.Length;
+Console.WriteLine($"Число физических строк - {totalLines}");
+
+var logicalLines = codeLines.Sum(line => RegexHelper.LogicalOperatorsRegex().Count(line));
+Console.WriteLine($"Число логических строк - {logicalLines}");
+
+var commentRowsCount = codeLines.Count(line => line.Contains("//"));
+Console.WriteLine($"Число строк коментариев - {commentRowsCount}");
+
+var levelOfCodeCommentability = commentRowsCount / (double)totalLines * 100;
+Console.WriteLine($"Уровень комментируемости кода - {levelOfCodeCommentability}%");
+partial class RegexHelper
+{
+    [GeneratedRegex("(((for)|(if)|(elif))\\s+\\(.+\\))|(\\S.+;)|(else)")]
+    public static partial Regex LogicalOperatorsRegex();
+}
